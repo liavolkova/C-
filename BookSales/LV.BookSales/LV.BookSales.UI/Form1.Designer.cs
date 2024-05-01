b2738cd26c1dc2539208ca29658e3268d6033e1e
Liana Volkova <125112820+liavolkova@users.noreply.github.com>
2024-04-30 22:42:44 -0500

    Add files via upload

---
+++ BookSales/LV.BookSales/LV.BookSales.UI/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace LV.BookSales.UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSalesAmount = new System.Windows.Forms.TextBox();
+            this.lblSalesAmount = new System.Windows.Forms.Label();
+            this.lblDiscount = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtDiscountAmount = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSalesAmount
+            // 
+            this.txtSalesAmount.Location = new System.Drawing.Point(292, 112);
+            this.txtSalesAmount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSalesAmount.Multiline = true;
+            this.txtSalesAmount.Name = "txtSalesAmount";
+            this.txtSalesAmount.Size = new System.Drawing.Size(204, 36);
+            this.txtSalesAmount.TabIndex = 1;
+            this.txtSalesAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtSalesAmount.TextChanged += new System.EventHandler(this.txtSalesAmount_TextChanged);
+            // 
+            // lblSalesAmount
+            // 
+            this.lblSalesAmount.AccessibleName = "";
+            this.lblSalesAmount.BackColor = System.Drawing.Color.White;
+            this.lblSalesAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalesAmount.Location = new System.Drawing.Point(124, 112);
+            this.lblSalesAmount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSalesAmount.Name = "lblSalesAmount";
+            this.lblSalesAmount.Size = new System.Drawing.Size(160, 37);
+            this.lblSalesAmount.TabIndex = 0;
+            this.lblSalesAmount.Text = "Sales Amount";
+            this.lblSalesAmount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblSalesAmount.UseCompatibleTextRendering = true;
+            // 
+            // lblDiscount
+            // 
+            this.lblDiscount.BackColor = System.Drawing.Color.White;
+            this.lblDiscount.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiscount.Location = new System.Drawing.Point(124, 219);
+            this.lblDiscount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDiscount.Name = "lblDiscount";
+            this.lblDiscount.Size = new System.Drawing.Size(160, 37);
+            this.lblDiscount.TabIndex = 0;
+            this.lblDiscount.Text = "Discount Amount";
+            this.lblDiscount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.BackColor = System.Drawing.Color.White;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(124, 270);
+            this.lblTotal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(160, 37);
+            this.lblTotal.TabIndex = 0;
+            this.lblTotal.Text = "Total";
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtDiscountAmount
+            // 
+            this.txtDiscountAmount.Location = new System.Drawing.Point(292, 219);
+            this.txtDiscountAmount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtDiscountAmount.Multiline = true;
+            this.txtDiscountAmount.Name = "txtDiscountAmount";
+            this.txtDiscountAmount.ReadOnly = true;
+            this.txtDiscountAmount.Size = new System.Drawing.Size(204, 36);
+            this.txtDiscountAmount.TabIndex = 6;
+            this.txtDiscountAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.txtTotal.Location = new System.Drawing.Point(292, 270);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTotal.Multiline = true;
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(204, 36);
+            this.txtTotal.TabIndex = 7;
+            this.txtTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnCalculate.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnCalculate.Location = new System.Drawing.Point(140, 170);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(108, 33);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnClear.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnClear.Location = new System.Drawing.Point(256, 170);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(108, 33);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnExit.Location = new System.Drawing.Point(372, 170);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(108, 33);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(663, 400);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblDiscount);
+            this.Controls.Add(this.lblSalesAmount);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtDiscountAmount);
+            this.Controls.Add(this.txtSalesAmount);
+            this.ForeColor = System.Drawing.Color.DarkRed;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "FVTC Book Sales";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtSalesAmount;
+        private System.Windows.Forms.Label lblSalesAmount;
+        private System.Windows.Forms.Label lblDiscount;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtDiscountAmount;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ BookSales/LV.BookSales/LV.BookSales.UI/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LV.BookSales.UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtSalesAmount.Focus();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double Sale = double.Parse(txtSalesAmount.Text);
+                const double DISCOUNT = 0.25; // constant value
+                double DiscountAmount = Sale * DISCOUNT; //calculate the discount amount
+                double Total = Sale - DiscountAmount; // calculate price after the discount
+                txtDiscountAmount.Text = DiscountAmount.ToString("c"); 
+                txtTotal.Text = Total.ToString("c");
+                txtSalesAmount.Focus();
+            }
+            catch
+            {
+                MessageBox.Show("Use numbers only.");
+                txtSalesAmount.Focus();
+                txtSalesAmount.SelectAll();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtDiscountAmount.Text = "";
+            txtSalesAmount.Text = "";
+            txtTotal.Text = "";
+            txtSalesAmount.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void txtSalesAmount_TextChanged(object sender, EventArgs e)
+        {
+            txtDiscountAmount.Text = "";
+            txtTotal.Text = "";
+        }
+
+    }
+}
