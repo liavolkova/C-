4cc3836334c5aea24562728e0b6ed1bfcc5bc330
Liana Volkova <125112820+liavolkova@users.noreply.github.com>
2024-04-30 22:43:42 -0500

    Add files via upload

---
+++ LV.MilesConverter/LV.Practice/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace LV.Practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMiles = new System.Windows.Forms.Label();
+            this.txtMiles = new System.Windows.Forms.TextBox();
+            this.lblKm = new System.Windows.Forms.Label();
+            this.txtKm = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMiles
+            // 
+            this.lblMiles.AutoSize = true;
+            this.lblMiles.Location = new System.Drawing.Point(82, 71);
+            this.lblMiles.Name = "lblMiles";
+            this.lblMiles.Size = new System.Drawing.Size(31, 13);
+            this.lblMiles.TabIndex = 0;
+            this.lblMiles.Text = "Miles";
+            // 
+            // txtMiles
+            // 
+            this.txtMiles.Location = new System.Drawing.Point(166, 68);
+            this.txtMiles.Name = "txtMiles";
+            this.txtMiles.Size = new System.Drawing.Size(144, 20);
+            this.txtMiles.TabIndex = 1;
+            this.txtMiles.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblKm
+            // 
+            this.lblKm.AutoSize = true;
+            this.lblKm.Location = new System.Drawing.Point(83, 111);
+            this.lblKm.Name = "lblKm";
+            this.lblKm.Size = new System.Drawing.Size(55, 13);
+            this.lblKm.TabIndex = 0;
+            this.lblKm.Text = "Kilometers";
+            // 
+            // txtKm
+            // 
+            this.txtKm.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtKm.Location = new System.Drawing.Point(166, 104);
+            this.txtKm.Name = "txtKm";
+            this.txtKm.Size = new System.Drawing.Size(144, 20);
+            this.txtKm.TabIndex = 2;
+            this.txtKm.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnConvert.Location = new System.Drawing.Point(80, 167);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(98, 24);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnClear.Location = new System.Drawing.Point(184, 167);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(98, 24);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnExit.Location = new System.Drawing.Point(288, 167);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(98, 24);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(415, 255);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtKm);
+            this.Controls.Add(this.lblKm);
+            this.Controls.Add(this.txtMiles);
+            this.Controls.Add(this.lblMiles);
+            this.Name = "Form1";
+            this.Text = " Convert Miles to Kilometers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMiles;
+        private System.Windows.Forms.TextBox txtMiles;
+        private System.Windows.Forms.Label lblKm;
+        private System.Windows.Forms.TextBox txtKm;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ LV.MilesConverter/LV.Practice/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LV.Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                double num1 = double.Parse(txtMiles.Text);//declaring and setting the value of num1
+                double num2 = 1.61; //setting the value of num2
+                double answer = num1 * num2; //multiplying
+                txtKm.Text = answer.ToString(); // storing the value in the answer(km) variable
+                txtMiles.Focus();
+
+            }
+            catch
+            {
+                MessageBox.Show("Please, enter numbers only.");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtKm.Text = "";
+            txtMiles.Text = "";
+            txtMiles.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
