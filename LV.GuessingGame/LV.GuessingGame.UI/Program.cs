7c78aecd03aa5b0d18f17a6cbdc52fff3b3dc677
Liana Volkova <125112820+liavolkova@users.noreply.github.com>
2024-04-30 22:50:50 -0500

    Add files via upload

---
+++ LV.GuessingGame/LV.GuessingGame.UI/Program.cs
@@ -0,0 +1,47 @@
+﻿string? playAgain = "yes";
+while (playAgain == "yes" || playAgain == "y")
+{
+
+    int min = 1;
+    int max = 100;
+    Random random = new Random();
+
+
+    int number = random.Next(min, max + 1); // 1-100
+    //Console.WriteLine("---->" + number);
+
+    int guess = 0;
+    int numberofGuesses = 0;
+
+    while (number != guess)
+    {
+        Console.Write("Guess a number: ");
+        string? input = Console.ReadLine();
+
+        if (int.TryParse(input, out guess) && guess >= min && guess <= max)
+        {
+            if (guess < number)
+            {
+                Console.WriteLine("My number is bigger");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("My number is smaller");
+            }
+        }
+    }
+    Console.WriteLine("Yes! " + number + " is my number");
+
+    if (numberofGuesses > 1)
+    {
+        Console.WriteLine("You've got it in " + numberofGuesses + " guesses");
+    }
+    else
+    {
+        Console.WriteLine("You've got it in 1 guess!");
+    }
+    
+ 
+    playAgain = Console.ReadLine()?.ToLower()?.Trim();
+
+}
