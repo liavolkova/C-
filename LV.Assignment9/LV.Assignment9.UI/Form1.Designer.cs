1096708f2051c9c92d94ba29f1f4978080fca83b
Liana Volkova <125112820+liavolkova@users.noreply.github.com>
2024-04-30 22:53:12 -0500

    Add files via upload

---
+++ LV.Assignment9/LV.Assignment9.UI/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace LV.Assignment9.UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDays = new System.Windows.Forms.Label();
+            this.txtDays = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lstReps = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDays
+            // 
+            this.lblDays.AutoSize = true;
+            this.lblDays.Location = new System.Drawing.Point(153, 96);
+            this.lblDays.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDays.Name = "lblDays";
+            this.lblDays.Size = new System.Drawing.Size(39, 16);
+            this.lblDays.TabIndex = 0;
+            this.lblDays.Text = "Days";
+            // 
+            // txtDays
+            // 
+            this.txtDays.Location = new System.Drawing.Point(267, 94);
+            this.txtDays.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDays.Name = "txtDays";
+            this.txtDays.Size = new System.Drawing.Size(167, 22);
+            this.txtDays.TabIndex = 1;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(76, 358);
+            this.btnCalc.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(148, 28);
+            this.btnCalc.TabIndex = 3;
+            this.btnCalc.Text = "&Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // lstReps
+            // 
+            this.lstReps.FormattingEnabled = true;
+            this.lstReps.ItemHeight = 16;
+            this.lstReps.Location = new System.Drawing.Point(122, 154);
+            this.lstReps.Margin = new System.Windows.Forms.Padding(4);
+            this.lstReps.Name = "lstReps";
+            this.lstReps.Size = new System.Drawing.Size(363, 148);
+            this.lstReps.TabIndex = 2;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(261, 358);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(148, 28);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click_1);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(446, 358);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(148, 28);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lstReps);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtDays);
+            this.Controls.Add(this.lblDays);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDays;
+        private System.Windows.Forms.TextBox txtDays;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.ListBox lstReps;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ LV.Assignment9/LV.Assignment9.UI/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LV.Assignment9.UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            lstReps.Items.Clear();
+            double days = 0;
+            double salary = 0.01;
+            double total=0;
+            double final = 0.01;
+
+            if (double.TryParse(txtDays.Text, out days) && days > 0)
+            {
+                total = salary;
+                
+                for (int i = 1; i <= days; i++)
+                {
+                    lstReps.Items.Add(i + ". " + total.ToString("C") + " " + final.ToString("C"));
+                    total *=2;
+                    final += total;
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Enter the whole numbers only.");
+            }
+        }
+
+        private void btnClear_Click_1(object sender, EventArgs e)
+        {
+            lstReps.Items.Clear();
+            txtDays.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
