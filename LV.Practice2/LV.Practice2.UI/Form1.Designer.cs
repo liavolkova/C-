264bc35cf751edae65467e33026b7f47c48a5807
Liana Volkova <125112820+liavolkova@users.noreply.github.com>
2024-04-30 22:45:15 -0500

    Add files via upload

---
+++ LV.Practice2/LV.Practice2.UI/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace LV.Practice2.UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCost = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.txtCost = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCost
+            // 
+            this.lblCost.AutoSize = true;
+            this.lblCost.Location = new System.Drawing.Point(116, 84);
+            this.lblCost.Name = "lblCost";
+            this.lblCost.Size = new System.Drawing.Size(28, 13);
+            this.lblCost.TabIndex = 0;
+            this.lblCost.Text = "Cost";
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(116, 121);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(46, 13);
+            this.lblQuantity.TabIndex = 0;
+            this.lblQuantity.Text = "Quantity";
+            // 
+            // txtCost
+            // 
+            this.txtCost.Location = new System.Drawing.Point(201, 75);
+            this.txtCost.Name = "txtCost";
+            this.txtCost.Size = new System.Drawing.Size(139, 20);
+            this.txtCost.TabIndex = 1;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(201, 114);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(139, 20);
+            this.txtQuantity.TabIndex = 1;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(106, 190);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(71, 34);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "Calculator";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 349);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtCost);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.lblCost);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCost;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.TextBox txtCost;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
