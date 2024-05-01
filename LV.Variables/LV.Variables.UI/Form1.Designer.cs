deb3385463683e9e0b7dea218ca81211222157c1
Liana Volkova <125112820+liavolkova@users.noreply.github.com>
2024-04-30 22:41:51 -0500

    Add files via upload

---
+++ LV.Variables/LV.Variables.UI/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace LV.Variables.UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(76, 125);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(71, 23);
+            this.txtName.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(75, 223);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(87, 37);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(186, 223);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(90, 37);
+            this.btnDisplay.TabIndex = 2;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Location = new System.Drawing.Point(77, 83);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(0, 15);
+            this.lblWelcome.TabIndex = 3;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(77, 165);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(71, 23);
+            this.txtAge.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 377);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.TextBox txtAge;
+    }
+}
+
---
+++ LV.Variables/LV.Variables.UI/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LV.Variables.UI
+{
+    public partial class Form1 : Form
+    {
+        //class variables (fields)
+        string firstName = "";
+        int age = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            firstName = txtName.Text;
+            txtName.Text = "";
+            // lblWelcome.Text = firstName;
+
+            age = int.Parse(txtAge.Text);
+            txtAge.Text = "";
+            //age = age + 1;
+            //lblWelcome.Text = "Welcome " + firstName + "(" + age.ToString()+")";
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            lblWelcome.Text = "Welcome" + firstName + "(" + age.ToString() + ")";
+        }
+    }
+}
